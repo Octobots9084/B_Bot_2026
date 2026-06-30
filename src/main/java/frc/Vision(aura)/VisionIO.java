ec4e6d6c3729f84a62399e2a787ec3a89e5a6807
sarfie <110944623+sarfie@users.noreply.github.com>
2026-06-29 20:55:06 -0700

    i blame seb

--- src/main/java/frc/Vision(aura)/VisionIO.java
+++ src/main/java/frc/Vision(aura)/VisionIO.java
@@ -1,14 +1,14 @@
-import java.util.ArrayList;
+// import java.util.ArrayList;
 
-public interface VisionIO {
-    public default boolean CameraConnected(int camera){
-        return false;
-    }
+// public interface VisionIO {
+//     public default boolean CameraConnected(int camera){
+//         return false;
+//     }
 
-     public default boolean CamerasConnected(){
-        return false;
-    }
+//      public default boolean CamerasConnected(){
+//         return false;
+//     }
 
-    public default void sendResults(ArrayList<FilteredResults[]> selectedResults){
-    }
-}
+//     public default void sendResults(ArrayList<FilteredResults[]> selectedResults){
+//     }
+// }
