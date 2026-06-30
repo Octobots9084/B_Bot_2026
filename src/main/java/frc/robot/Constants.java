7015855019a373e609ed9078391b79e6bfc11562
sarfie <110944623+sarfie@users.noreply.github.com>
2026-06-29 19:43:10 -0700

    get this working

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,12 @@
+package frc.robot;
+
+import com.ctre.phoenix6.CANBus;
+
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
+
+public class Constants {
+    
+      public static CANBus krakenBus = new CANBus("krakenbus");
+  public static Alliance allianceCOLOR;
+
+}
