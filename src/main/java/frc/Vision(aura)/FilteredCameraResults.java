04fe22471e26adaf0a9f061e65f32a2a48ae1427
SebastianBlackwell <sebastian.j.blackwell@gmail.com>
2026-07-01 11:18:31 -0700

    writing filteredCameraResults

--- src/main/java/frc/Vision(aura)/FilteredCameraResults.java
+++ src/main/java/frc/Vision(aura)/FilteredCameraResults.java
@@ -1,20 +1,46 @@
 public class FilteredCameraResults {
     /*
-    type: if it sees no tag whole thing is null
+    type: singletag, multitag, notargets
     photonTrackedTargets: all the information about one target/tag
     EstimatedRobotPose: the pose of the robot
+    PhotonPipelineResult: all the information from one reading
     */
 
-    //
+    //enum type;
+    //List<photonTrackedTargets> targets = new ArrayList();
+    //EstimatedRobotPose visionEst;
+    //PhotonPipelineResult result;
 
-    //public FilteredResults(reading){
-        //PhotonTrackedTarget = reading.getTarget;
-        //EstimatedRobotPose = reading.estimatePose;
-        //if(reading.estimateCoprocMultiTagPose.isNull()){
+    //constructor takes in a raw result
+    //public FilteredResults(result){
+        //result = this.result;
+
+        //the list of targets from the result gets filled
+        //for(int i = 0; i< result.getTargets().size(); i++){
+            //targets.add((result.getTargets()).get(i));
+        //}        
+
+        //if there are no targets this result has no targets
+        //if(targets.size() == 0){
+            //type = notargets;
+
+        //only happens if there are targets but you can't calculate a multitag result
+        //}else if(photonEstimator.estimateCoprocMultiTagPose(result).isNull()){
             //type = singletag;
         //}
+
+        //otherwise multitag
         //else{
             //type = multitag;
+        //}
+
+        //calculate pose
+        //if(type == multitag){
+            //visionEst = photonEstimator.estimateCoprocMultiTagPose(result);
+        //}
+
+        //if(type == singleTag){
+            //visionEst = photonEstimator.estimateLowestAmbiguityPose(result);
         //}
     //}
 
